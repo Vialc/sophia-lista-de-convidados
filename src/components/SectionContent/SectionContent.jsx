c28994a2de4816469381de56912c0708d56baf57
Vitor <costec222@gmail.com>
2022-07-21 20:36:38 -0600

    :spakles: feat: Responsividade e confirmação de presença

--- src/components/SectionContent/SectionContent.jsx
+++ src/components/SectionContent/SectionContent.jsx
@@ -1,32 +1,49 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import "./SectionContent.css";
 
 export function SectionContent() {
-  const [guestName, setGuestName] = useState('Vitor')
+  const [guestName, setGuestName] = useState("");
+  const [fetchingGuest, setFetchingGuest] = useState(true);
+  const [thanksMessage, setThanksMessage] = useState(false);
 
-  const params = useParams()
+  const params = useParams();
 
   function handleConfirmation() {
-    //axios.patch(`https://backend-sophia-guests-production.up.railway.app/guests/${params.id}`)
+    axios.patch(`https://backend-sophia-guests-production.up.railway.app/guests/${params.guestId}`, {
+      confirm: 'sim'
+    })
 
-    console.log(params)
+    setThanksMessage(true)
+
   }
 
   useEffect(() => {
-    axios.get(`https://cors-anywhere.herokuapp.com/https://backend-sophia-guests-production.up.railway.app/guests/${params.guestId}`)
+    axios
+      .get(
+        `https://backend-sophia-guests-production.up.railway.app/guests/${params.guestId}`
+      )
       .then((res) => {
         setGuestName(res.data.name);
-      })
-  
-  }, [])
-  
+        setFetchingGuest(false);
+      });
+  }, []);
+
   return (
     <div className="content-area__body">
-      <h3>Oi {guestName}.</h3>
-      <h3>Para viver as emoções desse dia tão importante, quero estar ao lado de pessoas especiais como você. Conto com a sua presença!</h3>
-      <button onClick={handleConfirmation}>Confirmar Presença</button>
+      {fetchingGuest ? (
+        <h3>Caregando..</h3>
+      ) : thanksMessage ? (<><h3>Obrigado por confirmar sua presença!</h3></>) : (
+        <>
+          <h3>Oi {guestName}.</h3>
+          <h3>
+            Para viver as emoções desse dia tão importante, quero estar ao lado
+            de pessoas especiais como você. Conto com a sua presença!
+          </h3>
+          <button onClick={handleConfirmation}>Confirmar Presença</button>
+        </>
+      )}
     </div>
   );
 }
--- src/components/SectionHeader/SectionHeader.jsx
+++ src/components/SectionHeader/SectionHeader.jsx
@@ -1,9 +1,11 @@
 import crown from '../../assets/images/crown2.png'
+import adorno from '../../assets/images/adorno.png'
 import "./SectionHeader.css";
 
 export function SectionHeader() {
   return <div className="content-area__header">
     <img  src={crown} alt="" />
     <p>Sophia</p>
+    <img className='adorno'  src={adorno} alt="" />
   </div>;
 }
